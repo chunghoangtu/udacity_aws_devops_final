8f244b518638b465f1e529d2f03267f0b517fe76
DESKTOP-CNDF47Q\chunghoangtu <chunghoangtu@gmail.com>
2023-08-06 11:32:02 +0700

    Fix lint failed and upload failed screenshot

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/home')
 def index():
-return 'Welcome. This is Udacity AWS DevOPS Capstone Project of TuChung'
+    return 'Welcome. This is Udacity AWS DevOPS Capstone Project of TuChung'
 
 app.run(host='0.0.0.0', port=80)
